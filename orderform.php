b12588f3e3d8f8184ad77077f0dadd43c0f883ad
cxb19188 <james.tierney.2019@uni.strath.ac.uk>
2021-11-04 12:51:06 +0000

    OrderForm page tidied up but could take form to new page when order is submitted?

--- orderform.php
+++ orderform.php
@@ -11,13 +11,12 @@
 </head>
 <body>
 <?php
-    //require("artlisting.php");
     $host = "devweb2021.cis.strath.ac.uk";
     $user = "cxb19188";
     $pass = "lohqu1PhaeSh";
     $dbname = $user;
     $conn = new mysqli($host, $user, $pass, $dbname);
-    session_start();
+    //session_start();
     $idVal = "";
 
     function safePOST($conn, $fieldName)
@@ -29,72 +28,26 @@
         }
     }
 
-//echo "id = ".$_GET['id'];
-    //$idVal = $_POST['button'];
+
     if( ($_SERVER['REQUEST_METHOD'] === "POST") && (!isset($_POST['submit'])) ) {
         $idVal = $_POST['idValue'];
-        echo "id Value is supposed to = " . $idVal;
     }
     else if( ($_SERVER['REQUEST_METHOD'] === "POST") && (isset($_POST['submit'])) ) {
         $idVal = $_POST['IDValue'];
-        echo "id Value is supposed to =========== " . $idVal;
-        //$sql = "SELECT 'Name' FROM `ArtSystem` WHERE 'Name' = 'The forrest'";//"SELECT * FROM `ArtSystem`"; select name where id = idVal
-        //$result = mysqli_query($conn, $sql);
-        //$result = $conn->query($sql);
         $name = $conn->query("SELECT Name FROM `ArtSystem` WHERE ID = $idVal")->fetch_object()->Name;
-        //echo "Result = ".$result[0];
-        //$row = $result->fetch_assoc();
-        echo "Name of painting selected to order was " . $name;//$row[0];
 
         // safely get all values to be inserted into the db
         $dbName = safePOST($conn, "name");
         $dbPhoneNum = safePOST($conn, "phone_number");
         $dbEmail = safePOST($conn, "email");
         $dbAddress = safePOST($conn, "address");
-        echo "name = ".$name."<br>";
-        echo "id vallllo = ".$idVal."<br>";
-        echo "first name = ".$dbName."<br>";
-        echo "number = ".$dbPhoneNum."<br>";
-        echo "email = ".$dbEmail."<br>";
-        echo "address = ".$dbAddress."<br>";
-        //$sql = "INSERT INTO `ArtOrders` ('Name', 'Phone_num', 'Email', 'Address', 'Painting_name', 'ID') VALUES"
-          //  . "($dbName, $dbPhoneNum, $dbEmail, $dbAddress, $name, $idVal)";
-
+        echo "Order Placed";
         $sql = "INSERT INTO `ArtOrders` (`Name`, `Phone_num`, `Email`, `Address`, `Painting_name`, `ID`) VALUES ('$dbName', '$dbPhoneNum', '$dbEmail', '$dbAddress', '$name', '$idVal')";
 
         $result = mysqli_query($conn, $sql);
-        echo $result;
-        if($result) {
-            echo "SUCCESSFUL INSERT";
-        }
-        else {echo "UNSUCESSFUL INSERT";}
+
 
     }
-       /* $sql = "SELECT 'Name' FROM `ArtSystem` WHERE 'Name' = 'The forrest'";//"SELECT * FROM `ArtSystem`"; select name where id = idVal
-        //$result = mysqli_query($conn, $sql);
-        //$result = $conn->query($sql);
-        $name = $conn->query("SELECT Name FROM `ArtSystem` WHERE ID = $idVal")->fetch_object()->Name;
-        //echo "Result = ".$result[0];
-        //$row = $result->fetch_assoc();
-        echo "Name of painting selected to order was " . $name;//$row[0];
-
-    // safely get all values to be inserted into the db
-        $dbName = safePOST($conn, "name");
-        $dbPhoneNum = safePOST($conn, "phone_number");
-        $dbEmail = safePOST($conn, "email");
-        $dbAddress = safePOST($conn, "address");
-        echo "name = ".$name;
-        echo "id vallllo = ".$idVal;
-            */
-    if (($_SERVER['REQUEST_METHOD'] === "POST") && (isset($_POST['submit']))) {
-        echo "in here";
-    //$sql = "INSERT INTO `ArtOrders` ('Name', 'Phone_num', 'Email', 'Address', 'Painting_name', 'ID') VALUES"
-        //. "($dbName, $dbPhoneNum, $dbEmail, $dbAddress, $name, $idVal)";
-    }
-
-
-//mysqli_close($conn);
-
 
 // what happens if we set hidden id val then set it = to id val from before and submit that val check if method === post and submit is set then new var for id = POST[IDValue]? and then select the name
 ?>
@@ -139,62 +92,6 @@
 
 </form>
 
-<?php /*if(($_SERVER["REQUEST_METHOD"] === "POST") && (($_POST['submit'] === 'Submit')) ) {
-    $id = $_POST['IDValue'];
-    $name = $_POST['paintName'];
-    echo "ID ---------> = ".$id;
-}*/ ?>
-
-<?php   // maybe we do need to take it to the other file and get our values from the form and open the db conn again and then insert from there
-    //}
-    /*
-// we can do what we done if the form failed so that if it is being submitted does this else
-    else {
-        function safePOST($conn, $fieldName)
-        {
-            if (isset($_POST[$fieldName])) {
-                return $conn->real_escape_string(strip_tags($_POST[$fieldName]));
-            } else {
-                return "";
-            }
-        }
-
-        $conn = new mysqli($host, $user, $pass, $dbname);
-        // safely get all values to be inserted into the db
-        $dbName = safePOST($conn, "name");
-        $dbPhoneNum = safePOST($conn, "phone_number");
-        $dbEmail = safePOST($conn, "email");
-        $dbAddress = safePOST($conn, "address");
-        $name = $_POST['paintName'];
-        $idVal =  $_POST['IDValue'];
-        echo "name = ".$name;
-        echo "id vallllo = ".$idVal;
-
-        if (($_SERVER['REQUEST_METHOD'] === "POST") && (isset($_POST['submit']))) {
-            $sql = "INSERT INTO `ArtOrders` ('Name', 'Phone_num', 'Email', 'Address', 'Painting_name', 'ID') VALUES"
-                . "($dbName, $dbPhoneNum, $dbEmail, $dbAddress, $name, $idVal)";
-
-            //  $sql = "INSERT INTO `ArtOrders`(`Name`, `Phone_num`, `Email`, `Address`, `Painting_name`, `ID`) VALUES ('$dbName', '$dbPhoneNum', '$dbEmail', '$dbAddress', 'hello world', '00120')";
-            echo "form submitted";
-            // carries out our query
-            $result  = mysqli_query($conn, $sql);
-            if($result) {
-                echo "SUCCESSFUL INSERT";
-            }
-            else {echo "UNSUCESSFUL INSERT";}
-            // carries out our query
-
-
-            $sql = "INSERT INTO `wadFraming` (`width`, `height`, `postage`, `email`, `price`, `date_requested`) VALUES"
-            ."('$dbWidth', '$dbHeight', '$dbPostage', '$dbEmail', '$priceNoVat', now())";
-
-            // carries out our query
-            mysqli_query($conn, $sql);
-
-
-        }
-    } */
-?>
 </body>
 </html>
 
