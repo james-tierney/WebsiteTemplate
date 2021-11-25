822c6f06d6132b9fa45fb77b7a5eb0db56bad621
cxb19188 <james.tierney.2019@uni.strath.ac.uk>
2021-11-25 18:10:34 +0000

    headerNav file done and details

--- details.php
+++ details.php
@@ -4,16 +4,41 @@
     <meta charset="UTF-8">
     <title>Art Listing</title>
     <link href=table.css rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
 
-    <!--<script> type="text/javascript" src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
-    <script> type="text/javascript" src="https://cdn.datatables.net/1.10.12/js/jquery/datatables.min.js"></script>
 
-    <script>
-        $(document).ready(function () {
-            $("#table1").DataTable();
-        });
-    </script> -->
+    <style>
 
+
+        .group {
+            width: 100%;
+            height: 100%;
+            justify-content: center;
+        }
+        .details {
+            margin: 1rem;
+            padding: 1rem;
+            justify-content: center;
+            display: flex;
+
+        }
+        .box p {
+            font-weight: bold;
+            font-size: 4rem;
+        }
+
+        .images {
+            justify-content: center;
+            display: flex;
+        }
+        .images img  {
+            width: 66%;
+            height: 66%;
+            justify-content: center;
+
+            display: flex;
+        }
+    </style>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 
@@ -29,7 +54,7 @@
         });
     </script>
 </head>
-<body style="background-color: cornflowerblue">
+<body style="background: #202020;">
 
 <script>
 
@@ -42,20 +67,6 @@
 
     }
 
-    function setVal() {
-        return document.getElementById('orderID').value = myFunc();
-
-    }
-
-    function myFunc(flag, buttonID) {
-
-        //let id = buttonID.getAttribute('data-id');
-        let id = document.getElementById("myButton").value;
-        console.log("The value of the id from the doc.getId = " + id);
-        if(flag === true) {
-        }
-        return id;
-    }
 
     function redirect(url) {
         location.href = url;
@@ -71,9 +82,13 @@
 
 </script>
 
-<!--style the back button to bottom left of the page -->
-<button id="backButton" name="back" onclick=redirect("artlisting.php")>Back</button>
 
+<div id="backButton">
+    <button class=detailsButton name="back" onclick=redirect("listart.php")>Back</button>
+</div>
+<div id="detailsHeader">
+    <h1 >Details About Painting</h1>
+</div>
 <?php
 // Connect to MySQL
 $host = "devweb2021.cis.strath.ac.uk";
@@ -83,51 +98,58 @@ $dbname = $user;
 $conn = new mysqli($host,$user,$pass,$dbname);
 
 
-$sql = "SELECT * FROM `ArtSystem`";
+$idVal = $_POST["val"];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['idButton'])) {
+    $idVal = $_POST['val'];
+
+
+$sql = "SELECT * FROM `ArtSystem` WHERE ID = $idVal";
 $result = $conn->query($sql);
 if($result->num_rows > 0) {
-    echo "<table id='table1' onclick='handleClick(event)' class='styled-table'>\n";
-    echo "<b> </b><thead>";
-    echo "<b> <tr class='active-row'>";
-    echo "<td><b>Name</td>";
-    echo "<td><b>Completion Date</td>";
-    echo "<td><b>Width</td>";
-    echo "<td><b>Height</td>";
-    echo "<td><b>Price</td>";
-    echo "<td><b>Description</td>";
-    echo "<td><b>ID</td>";
-    echo "<td><b>Thumbnail</td>";
-    echo "<td><b>Order Button</td>";
-    echo "<b></tr>";
 
 
-    echo "<b></thead>";
     $count = 0;
     while ($row = $result->fetch_assoc()) {
         $image = "";
-        if(isset($row['thumbnail'])) {
-            $image = "<img src='data:image/jpeg;base64,".base64_encode($row['thumbnail'])."'/>";
+        if (isset($row['thumbnail'])) {
+            $image = "<img src='data:image/jpeg;base64," . base64_encode($row['thumbnail']) . "'/>";
         }
-        echo "<p>";
-        echo "<b><tr>\n";
-        echo "<td>".$row['Name']."</td>\n";
-        echo "<td>".$row['date_of_completion']."</td>\n";
-        echo "<td>".$row['Width']."</td>\n";
-        echo "<td>".$row['Height']."</td>\n";
-        echo "<td>".$row['Price']."</td>\n";
-        echo "<td>".$row['Description']."</td>\n";
-        echo "<td>".$row['ID']."</td>\n";
-        //echo "<td>".$row['thumbnail']."</td>\n";
-        echo "<td>".$image."</td>\n";
-        //echo "<img src='data:image/jpeg;base64,".base64_encode($row['thumbnail'])."'/>";
-        //echo "<td>".'<img src="data:image/jpeg;base64,'.base64_encode($row['thumbnail']->load()) .'" />'."</td>";
-        print '<td> <button id="button" name="idButton"<a href="artlisting.php?id=button" style="border-radius: 8px; background-color: #008CBA; " onclick="saveID(' .$row['ID'].')">Order</a>   </td>';
-        echo "</tr>\n";
-        echo "</p></b>";
+
+        echo '<div class="group">';
+        echo '<div class="images"> <img alt='.$row["Description"].' src="data:image/jpeg;base64,'.base64_encode($row['thumbnail']).' "/>';
+        echo '</div>';
+        echo '<div class="details">';
+        echo '<button id="button" class=detailsButton name="idButton" value="'.$row['ID'].'"<a href="listart.php?id=button" onclick="saveID(' . $row['ID'] . ')">Order</a>';
+        echo '</div>';
+        echo '<div class="description">';
+
+
+
+            echo '<div  class="box">';
+            echo '<p>'."Name - ".$row["Name"]."\n".'</p>';
+            echo '<p>'."Price - ".$row['Price']."\n".'</p>';
+            echo '<p>'."Width ".$row['Width']." x Height ".$row['Height']."\n".'</p>';
+            echo '</div>';
+
+            echo '<div  class="box">';
+
+            echo '<p>'."Description\n".'</p>';
+            echo '<p>'.$row['Description']."\n".'</p>';
+
+            echo '</div>';
+
+
+
+        echo '</div>';
+
+
+
 
     }
     echo "</table>\n";
     mysqli_close($conn);
+    }
 }
 
 $conn = new mysqli($host,$user,$pass,$dbname);
@@ -138,18 +160,10 @@ if( ($_SERVER["REQUEST_METHOD"] === "POST") && (isset($_POST['submit'])) ) {
     $dbEmail = strip_tags(isset($_POST['email']) ? $_POST['email'] : "");
     $dbAddress = strip_tags(isset($_POST['address']) ? $_POST['address'] : "");
 
-    echo "this post has went through";
-    echo isset($_POST['submit']);
-
     $sql = "INSERT INTO `ArtOrders`(`Name`, `Phone_num`, `Email`, `Address`, `Painting_name`, `ID`) VALUES ('$dbName', '$dbPhoneNum', '$dbEmail', '$dbAddress', 'hello world', '00120')";
-    echo "sql = ".$sql;
 
     $result = mysqli_query($conn, $sql);
-    echo $result;
-    if($result) {
-        echo "SUCCESSFUL INSERT";
-    }
-    else {echo "UNSUCESSFUL INSERT";}
+
     // carries out our query
 
 }
@@ -181,24 +195,12 @@ if( ($_SERVER["REQUEST_METHOD"] === "POST") && (isset($_POST['submit'])) ) {
 
 
 
-<!--
-    when form is clicked the data inserted within the form should be sent to the art orders database
--->
 
-<!--
-    Need to add code to insert the order into the database for orders when submit button is clicked
--->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
 
-    // Pagination jquery
-   /* $(document).ready(function () {
-        $('#table1').DataTable({
-            "paging": true
-            "pagingType": "simple"
-        });
-    }) */
+
 
     $(document).ready(function () {
         $("#order").click(function () {
